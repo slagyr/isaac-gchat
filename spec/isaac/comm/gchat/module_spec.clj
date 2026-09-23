bc219df543f70c06479348388c0db88269446031
Micah <micahmartin@gmail.com>
2026-09-23 06:16:11 -0700

    Release 0.2.4: the login requests chat.spaces.create — Yopp can open a DM and join one it was invited to

--- spec/isaac/comm/gchat/module_spec.clj
+++ spec/isaac/comm/gchat/module_spec.clj
@@ -23,11 +23,12 @@
     (should= 'isaac.comm.gchat.handler/handle-event
              (get-in manifest [:isaac.google/handler "google.workspace.chat.message.v1.created"])))
 
-  (it "contributes a Chat registration and the messages, spaces and memberships scopes"
+  (it "contributes a Chat registration and the messages, spaces, memberships and spaces.create scopes"
     (should= 'isaac.comm.gchat.registration/create!
              (get-in manifest [:isaac.google/registration :chat :create!]))
     (should= ["https://www.googleapis.com/auth/chat.messages"
               "https://www.googleapis.com/auth/chat.spaces.readonly"
-              "https://www.googleapis.com/auth/chat.memberships.readonly"]
+              "https://www.googleapis.com/auth/chat.memberships.readonly"
+              "https://www.googleapis.com/auth/chat.spaces.create"]
              (:isaac.google/scopes manifest)))
   )
